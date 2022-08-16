d6b425321f3c0e8828d45f5d53ce84487d26faf4
Ancestors45 <108401297+Ancestors45@users.noreply.github.com>
2022-08-16 19:00:51 +0800

    Update XyberArm.py

--- XyberArm.py
+++ XyberArm.py
@@ -55,7 +55,7 @@ def ___menu___():
     try:
         os.system('clear')
         print(___logo___)
-        ___roz = requests.get(f'https://i.instagram.com/api/v1/users/{open("Data/user.txt","r").read()}/info/', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie': open('Data/coki.txt','r').read()}).json()['user']
+        ___roz = requests.get(f'https://i.instagram.com/api/v1/users/{open("Data/user.txt","r").read()}/info/', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 4.4.2; SM-T230NU Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.84 Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie': open('Data/coki.txt','r').read()}).json()['user']
         print(f"{P}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
         print(f"{B}┃{P} You name instagram :{K} {___roz['full_name']}          ┃")
         print(f"{B}┃{P} Username instagram :{K} {___roz['username']} ┃")
